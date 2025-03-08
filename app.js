856649356249eb5ff1bd160c7a8481aaa10a9fa2
Adolfo Isai Ponce Velazquez <74275802+IsaiPonce1570@users.noreply.github.com>
2025-03-07 18:04:48 -0600

    Implementación de función amigos
    se implementan las primeras funciones en el JS así como los arreglos

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos[];
+
+function capturarValor(){
+    var valor=document.querySelector(".micampo").value;
+    
+    if(valor.trim()===""){
+        alert("Inserte un nombre, por favor.");
+    }else{
+        amigos.push(valor.trim());
+        
+        console.log("Nombre Ingresado:" * + valor);
+
+        console.log("Lista de amigos:",amigos);
+        
+        //Limpiar el campo de entrada
+        document.querySelector(".micampo".value="");
+    }
+}
